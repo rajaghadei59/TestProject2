473629a4dea8285bddf03fa0ccf7844cdca184f5
rajaghadei59 <57008155+rajaghadei59@users.noreply.github.com>
2020-02-24 23:32:16 +0530

    ganesh changed test class

--- TestProject2/src/main/java/com/app/TestProject2/Test.java
+++ TestProject2/src/main/java/com/app/TestProject2/Test.java
@@ -3,8 +3,7 @@ package com.app.TestProject2;
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("test class added");
 	}
 
 }
